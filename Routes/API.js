a3046bc529513466fbee6e924149680a8aa192b6
Nevoba <nevoba@gmail.com>
2022-08-18 00:18:16 +0300

    Bug fixes in route

--- Routes/API.js
+++ Routes/API.js
@@ -5,11 +5,23 @@ const router = express.Router();
 const BallotBox = require('../Models/BallotBox')
 
 //Routes
-router.get('/api', (req,res) => {
+
+router.get('/name', (req,res) => {
+    console.log('Snitch');
+    const data = {
+        username: 'Nevo',
+        age: 22
+    };
+    res.json(data);
+});
+
+
+
+router.get('/', (req,res) => {
 
     BallotBox.find({ })
     .then((data) => {
-        console.log('Data:', data);
+        console.log('BAE');
         res.json(data);
     })
     .catch((error)=> {
@@ -19,13 +31,7 @@ router.get('/api', (req,res) => {
 
 
 
-router.get('/api/name', (req,res) => {
-    const data = {
-        username: 'Nevo',
-        age: 22
-    };
-
-});
 
 
-module.exports = router
+
+module.exports = router;
