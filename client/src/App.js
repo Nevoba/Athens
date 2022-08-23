753b873a9ec6bb2788d6b5903bc32e90a8c11716
Nevoba <nevoba@gmail.com>
2022-08-23 22:26:20 +0300

    From laptop with dependancies

--- client/src/App.js
+++ client/src/App.js
@@ -4,28 +4,6 @@ import { allCountries } from './domain/listOfCountries';
 import { callPostfixs } from './domain/eliminationCalls';
 
 
-/* const { MongoClient } = require("mongodb");
-const uri =
-  "mongodb+srv://Cluster23486:SElddn1IenFg@cluster23486.7gio09e.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir); */
-
-a
-
-
 class App extends React.Component{
     
     constructor(props){
